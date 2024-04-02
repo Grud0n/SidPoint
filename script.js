74b398a5f4d0f47f2d67f8d48aead37b725c548f
MasterYeet04 <86265357+MasterYeet04@users.noreply.github.com>
2024-04-02 11:53:29 +0200

    Implementet Change Function

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-function showMessage(id) {
+/*function showMessage(id) {
     alert(id);
+}*/
+
+// Current visible section
+let currentSection = 1;
+
+function showMessage(section) {
+  // Hide the current section
+  document.getElementById(currentSection.toString()).classList.add("hidden");
+
+  if (section === "Left") {
+    // Go to the previous section
+    currentSection = currentSection > 1 ? currentSection - 1 : 10;
+  } else if (section === "Right") {
+    // Go to the next section
+    currentSection = currentSection < 10 ? currentSection + 1 : 1;
+  } else {
+    // Go to the specified section
+    currentSection = section;
+  }
+
+  // Show the new current section
+  document.getElementById(currentSection.toString()).classList.remove("hidden");
 }
+
+// When the page loads, show the first section
+window.onload = function() {
+  showMessage(1);
+};
+
+
